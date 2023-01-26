33ab7f7dfb5887e7ffe6f59b7632bd765d9705f5
hboppe <hanna.nascimento31@gmail.com>
2023-01-26 12:24:47 -0500

    style: added style to landing page top container

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -5,12 +5,12 @@ function LandingPage() {
     return (
         <div className={styles.landingPageContainer}>
             <div className={styles.topContainer}>
-                <img src={logo} alt="" />
+                <img src={logo} alt="Nu Kenzie logo" />
 
-                    <h1>Centralize control of your finances</h1>
-                
-                
+                <h1>Centralize control of your finances</h1>
+
                 <p>in a faster and secure way</p>
+                
                 <button type='button'>Start</button>
             </div>
             <div className={styles.bottomContainer}>
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.module.css'
-import Font from 'react-font'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
