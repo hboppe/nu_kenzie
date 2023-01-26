4c15fd79e7f2d1ea04c5994c867ad38d82d973c0
hboppe <hanna.nascimento31@gmail.com>
2023-01-26 13:12:02 -0500

    style: add media queries to landing page

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -3,18 +3,19 @@ import logo from '../../assets/logo.svg';
 
 function LandingPage() {
     return (
-        <div className={styles.landingPageContainer}>
-            <div className={styles.topContainer}>
-                <img src={logo} alt="Nu Kenzie logo" />
 
-                <h1>Centralize control of your finances</h1>
-
-                <p>in a faster and secure way</p>
+        <div className={styles.landingPage}>
+            <div className={styles.landingPageContainer}>
+                <div className={styles.topContainer}>
+                    <img src={logo} alt="Nu Kenzie logo" />
+                    <h1>Centralize control of your finances</h1>
+                    <p>in a faster and secure way</p>
                 
-                <button type='button'>Start</button>
-            </div>
-            <div className={styles.bottomContainer}>
+                    <button type='button'>Start</button>
+                </div>
+                <div className={styles.bottomContainer}>
                 
+                </div>
             </div>
         </div>
       
