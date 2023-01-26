76cc4edc14a6c4d18e76df47af1e0adb08f34caf
hboppe <hanna.nascimento31@gmail.com>
2023-01-26 10:38:16 -0500

    feat: created landing page component

---
+++ src/components/LandingPage/index.jsx
@@ -0,0 +1,27 @@
+import styles from './style.module.css';
+import logo from '../../assets/logo.svg';
+
+function LandingPage() {
+    return (
+        <div className={styles.landingPageContainer}>
+            <div className={styles.topContainer}>
+                <img src={logo} alt="" />
+
+                    <h1>Centralize control of your finances</h1>
+                
+                
+                <p>in a faster and secure way</p>
+                <button type='button'>Start</button>
+            </div>
+            <div className={styles.bottomContainer}>
+                
+            </div>
+        </div>
+      
+        
+            
+       
+    )
+}
+
+export default LandingPage;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.module.css'
+import Font from 'react-font'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <App />
+    
+      
   </React.StrictMode>,
 )
