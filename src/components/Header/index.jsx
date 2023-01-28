604f39be578b5d4c39185cfc085942c40429af6b
hboppe <hanna.nascimento31@gmail.com>
2023-01-27 20:40:32 -0500

    chore: add favicon to html file

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,20 @@
 import logo from '../../assets/dark-logo.svg'
 import styles from './style.module.css';
+import { useState } from 'react';
 
 function Header({click}) {
+
+    const [refreshPage, setRefreshPage] = useState(false);
 
     function GoToHome(callback){
         callback(false)
     }
+
     
     return (
         <header className={styles.headerContainer}>
             <nav className={styles.navContainer}>
-                <img src={logo} alt="Nu Kenzie logo" />
+                <img className={styles.logo} src={logo} alt="Nu Kenzie logo"/>
                 <button className={styles.homeButton} onClick={() => GoToHome(click)}>Home</button>
             </nav>
         </header>
